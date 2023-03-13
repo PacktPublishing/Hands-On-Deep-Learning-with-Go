1867aeeda15a8eb02636007c1e8044b202fd4fca
JPratama7 <josepratama080@gmail.com>
2023-03-13 19:26:02 +0700

    tugas 1

---
+++ Chapter01/A/1204004/tutorial1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorgonia.org/gorgonia"
+)
+
+func main() {
+	g := gorgonia.NewGraph()
+
+	var x, y, z *gorgonia.Node
+	var err error
+
+	// define the expression
+	x = gorgonia.NewScalar(g, gorgonia.Float64, gorgonia.WithName("x"))
+	y = gorgonia.NewScalar(g, gorgonia.Float64, gorgonia.WithName("y"))
+	if z, err = gorgonia.Add(x, y); err != nil {
+		log.Fatal(err)
+	}
+
+	// create a VM to run the program on
+	machine := gorgonia.NewTapeMachine(g)
+	defer machine.Close()
+
+	// set initial values then run
+	gorgonia.Let(x, 2.0)
+	gorgonia.Let(y, 2.5)
+	if err = machine.RunAll(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v", z.Value())
+}
