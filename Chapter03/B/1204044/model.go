c96f0b97f110cc5ca24af33d8f22e17a94f50aea
fahiraaa06 <fahira1678@gmail.com>
2023-04-09 22:03:32 +0700

    3-3B-1204044-Fahira

--- Chapter03/B/1204044/model.go
+++ Chapter03/B/1204044/model.go
@@ -6,18 +6,21 @@ import (
 	"fmt"
 	"image"
 	"image/jpeg"
+	"io/ioutil"
 	"log"
 	"math"
 	"math/rand"
 	"os"
-	"time"
+
+	_ "net/http/pprof"
 
 	"github.com/aiteung/mnist"
 	"github.com/pkg/errors"
 	"gopkg.in/cheggaaa/pb.v1"
-
 	"gorgonia.org/gorgonia"
 	"gorgonia.org/tensor"
+
+	"time"
 )
 
 var (
@@ -61,22 +64,11 @@ func (s sli) End() int   { return s.end }
 func (s sli) Step() int  { return 1 }
 
 func newNN(g *gorgonia.ExprGraph) *nn {
-	// Create node for w/weight
-
-	weight0, weight1, weight2, weight3, err := readFromBackup(backup + "backup1.gob")
-	if err != nil {
-		panic(err)
-	}
 
 	w0 := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(784, 128), gorgonia.WithName("w0"), gorgonia.WithInit(gorgonia.GlorotU(1.0)))
 	w1 := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(128, 64), gorgonia.WithName("w1"), gorgonia.WithInit(gorgonia.GlorotU(1.0)))
 	w2 := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(64, 128), gorgonia.WithName("w2"), gorgonia.WithInit(gorgonia.GlorotU(1.0)))
 	w3 := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(128, 784), gorgonia.WithName("w3"), gorgonia.WithInit(gorgonia.GlorotU(1.0)))
-
-	gorgonia.Let(w0, weight0)
-	gorgonia.Let(w1, weight1)
-	gorgonia.Let(w2, weight2)
-	gorgonia.Let(w3, weight3)
 
 	return &nn{
 		g:  g,
@@ -157,11 +149,6 @@ func main() {
 	parseDtype()
 	rand.Seed(7945)
 
-	// // intercept Ctrl+C
-	// sigChan := make(chan os.Signal, 1)
-	// signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	// doneChan := make(chan bool, 1)
-
 	var inputs tensor.Tensor
 	var err error
 
@@ -174,46 +161,39 @@ func main() {
 	numExamples := inputs.Shape()[0]
 	bs := *batchsize
 
-	// MNIST data consists of 28 by 28 black and white images
-	// however we've imported it directly now as 784 different pixels
-	// as a result, we need to reshape it to match what we actually want
-	// if err := inputs.Reshape(numExamples, 1, 28, 28); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// we should now also proceed to put in our desired variables
-	// x is where our input should go, while y is the desired output
 	g := gorgonia.NewGraph()
-	// x := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 1, 28, 28), gorgonia.WithName("x"))
+
 	x := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(bs, 784), gorgonia.WithName("x"))
 	y := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(bs, 784), gorgonia.WithName("y"))
 
-	// Init variables
-	m := newNN(g)
+	ioutil.WriteFile("simple_graph.dot", []byte(g.ToDot()), 0644)
 
+	m := newNN(g)
 	if err = m.fwd(x); err != nil {
 		log.Fatalf("%+v", err)
 	}
+
+	ioutil.WriteFile("simple_graph_2.dot", []byte(g.ToDot()), 0644)
 
 	losses, err := gorgonia.Square(gorgonia.Must(gorgonia.Sub(y, m.out)))
 	if err != nil {
 		log.Fatal(err)
 	}
 	cost := gorgonia.Must(gorgonia.Mean(losses))
+	// cost = gorgonia.Must(gorgonia.Neg(cost))
+
+	ioutil.WriteFile("simple_graph_3.dot", []byte(g.ToDot()), 0644)
+
+	// we wanna track costs
 	var costVal gorgonia.Value
 	gorgonia.Read(cost, &costVal)
 
-	// cost = gorgonia.Must(gorgonia.Neg(cost))
-	// create a VM to run the program on
-	machine := gorgonia.NewTapeMachine(g)
-	defer machine.Close()
-
-	if inputs, _, err = mnist.Load("test", loc, dt); err != nil {
+	if _, err = gorgonia.Grad(cost, m.learnables()...); err != nil {
 		log.Fatal(err)
 	}
 
-	numExamples = inputs.Shape()[0]
-	bs = *batchsize
+	vm := gorgonia.NewTapeMachine(g, gorgonia.BindDualValues(m.learnables()...))
+	solver := gorgonia.NewAdamSolver(gorgonia.WithBatchSize(float64(bs)), gorgonia.WithLearnRate(0.01))
 
 	batches := numExamples / bs
 	log.Printf("Batches %d", batches)
@@ -221,6 +201,70 @@ func main() {
 	bar.SetRefreshRate(time.Second / 20)
 	bar.SetMaxWidth(80)
 
+	for i := 0; i < *epochs; i++ {
+		// for i := 0; i < 10; i++ {
+		bar.Prefix(fmt.Sprintf("Epoch %d", i))
+		bar.Set(0)
+		bar.Start()
+		for b := 0; b < batches; b++ {
+			start := b * bs
+			end := start + bs
+			if start >= numExamples {
+				break
+			}
+			if end > numExamples {
+				end = numExamples
+			}
+
+			// var xVal, yVal tensor.Tensor
+			var xVal tensor.Tensor
+			if xVal, err = inputs.Slice(sli{start, end}); err != nil {
+				log.Fatal("Unable to slice x")
+			}
+
+			if err = xVal.(*tensor.Dense).Reshape(bs, 784); err != nil {
+				log.Fatal("Unable to reshape %v", err)
+			}
+
+			gorgonia.Let(x, xVal)
+			gorgonia.Let(y, xVal)
+			if err = vm.RunAll(); err != nil {
+				log.Fatalf("Failed at epoch  %d: %v", i, err)
+			}
+
+			arrayOutput := m.predVal.Data().([]float64)
+			yOutput := tensor.New(tensor.WithShape(bs, 784), tensor.WithBacking(arrayOutput))
+
+			for j := 0; j < 1; j++ {
+				rowT, _ := yOutput.Slice(sli{j, j + 1})
+				row := rowT.Data().([]float64)
+
+				img := visualizeRow(row)
+
+				f, _ := os.OpenFile(fmt.Sprintf("training/%d - %d - %d training.jpg", j, b, i), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+				jpeg.Encode(f, img, &jpeg.Options{jpeg.DefaultQuality})
+				f.Close()
+			}
+
+			// solver.Step(m.learnables())
+			solver.Step(gorgonia.NodesToValueGrads(m.learnables()))
+			vm.Reset()
+			bar.Increment()
+		}
+		bar.Update()
+		log.Printf("Epoch %d | cost %v", i, costVal)
+	}
+	bar.Finish()
+
+	log.Printf("Run Tests")
+
+	// load our test set
+	if inputs, _, err = mnist.Load("test", loc, dt); err != nil {
+		log.Fatal(err)
+	}
+
+	numExamples = inputs.Shape()[0]
+	bs = *batchsize
 	batches = numExamples / bs
 
 	bar = pb.New(batches)
@@ -244,19 +288,13 @@ func main() {
 			log.Fatal("Unable to slice x")
 		}
 
-		// if yVal, err = inputs.Slice(sli{start, end}); err != nil {
-		// 	log.Fatal("Unable to slice y")
-		// }
-		// if err = xVal.(*tensor.Dense).Reshape(bs, 1, 28, 28); err != nil {
-		// 	log.Fatal("Unable to reshape %v", err)
-		// }
 		if err = xVal.(*tensor.Dense).Reshape(bs, 784); err != nil {
 			log.Fatal("Unable to reshape %v", err)
 		}
 
 		gorgonia.Let(x, xVal)
 		gorgonia.Let(y, xVal)
-		if err = machine.RunAll(); err != nil {
+		if err = vm.RunAll(); err != nil {
 			log.Fatalf("Failed at epoch test: %v", err)
 		}
 
@@ -289,38 +327,28 @@ func main() {
 			f.Close()
 		}
 
-		machine.Reset()
+		vm.Reset()
 		bar.Increment()
 	}
+
+	save(m)
+
 	log.Printf("Epoch Test | cost %v", costVal)
-	bar.Finish()
+
 }
 
-func readFromBackup(file string) (w0 tensor.Dense, w1 tensor.Dense, w2 tensor.Dense, w3 tensor.Dense, err error) {
-	f, err := os.Open(file)
+func save(model *nn) error {
+	f, err := os.Create(backup + "backup1.gob")
 	if err != nil {
-		return
+		return err
 	}
 	defer f.Close()
-	dec := gob.NewDecoder(f)
-	log.Println("decoding xT")
-	err = dec.Decode(&w0)
-	if err != nil {
-		return
+	enc := gob.NewEncoder(f)
+	for _, node := range model.learnables() {
+		err := enc.Encode(node.Value())
+		if err != nil {
+			return err
+		}
 	}
-	log.Println("decoding yT")
-
-	err = dec.Decode(&w1)
-	if err != nil {
-		return
-	}
-	err = dec.Decode(&w2)
-	if err != nil {
-		return
-	}
-	err = dec.Decode(&w3)
-	if err != nil {
-		return
-	}
-	return
+	return nil
 }
